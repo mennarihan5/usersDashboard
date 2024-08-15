f30b3150f6e271c2fb123fef3a7b3fee66bacc17
mennarihan5 <menna1986@live.com>
2024-08-15 12:41:48 +0300

    finalized styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, UserDashboardComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'user-dashboard';
+  title = 'user-dashboard-app';
 }
